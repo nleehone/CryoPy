b42749f4c133f470cbb5642ad396012bc0d5426a
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-01-29 15:05:10 -0800

    Create temperature controller application
    The application displays the temperature of channel A and updates it every
    second.

--- temperature_controller.py
+++ temperature_controller.py
@@ -1,6 +1,31 @@
+import visa
 from drivers.LS350 import *
-import tkinter
+import tkinter as tk
+
+
+class TemperatureControllerApplication(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        rm = visa.ResourceManager('instruments.yaml@sim')
+        self.driver = LS350(rm.open_resource('ASRL1::INSTR'))
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.tempA = tk.StringVar()
+        self.tempALabel = tk.Label(self, font=('Helvetica', 24))
+        self.tempALabel.pack(side='top')
+        self.tempALabel['textvariable'] = self.tempA
+        self.quit = tk.Button(self, text='QUIT', fg='red', command=root.destroy)
+        self.quit.pack(side='bottom')
+        self.onUpdate()
+
+    def onUpdate(self):
+        self.after(1000, self.onUpdate)
+        self.tempA.set(self.driver.get_temperature('A'))
+
 
 if __name__ == '__main__':
-    top = tkinter.Tk()
-    top.mainloop()
+    root = tk.Tk()
+    app = TemperatureControllerApplication(master=root)
+    root.mainloop()
