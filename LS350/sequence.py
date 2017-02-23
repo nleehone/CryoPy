7521da66100edf512fa9b3dd9c92999d3c18a699
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2017-02-23 13:55:18 -0800

    Save data from sequence into file

--- LS350/sequence.py
+++ LS350/sequence.py
@@ -2,10 +2,36 @@ import pika
 from threading import Thread
 import time
 import json
+import h5py
+import numpy as np
 
 prefix = "LS350"
 SERVER_QUEUE = "{}.{}".format(prefix, "acquirer")
 response = None
+
+file_name = 'test.hdf5'
+file = h5py.File(file_name, 'w')
+file.attrs['file_name'] = file_name
+file.attrs['file_creation_time'] = time.strftime("%a, %d %b %Y %X +0000", time.gmtime())
+file.attrs['operator'] = 'Nicholas'
+file.attrs['sample'] = 'Test sample'
+file.attrs['comment'] = 'Comment'
+
+raw_data = file.create_group('raw_data')
+count = 0
+
+newtype = np.dtype([('Temp A', float),
+                    ('Temp B', float),
+                    ('Temp C', float),
+                    ('Temp D', float),
+                    ('Sens A', float),
+                    ('Sens B', float),
+                    ('Sens C', float),
+                    ('Sens D', float),
+                    ])
+
+t_ds = raw_data.create_dataset('temperature', (0,1), maxshape=(None, len(newtype)), dtype=newtype)
+t_ds.attrs['instrument'] = 'LS350'
 
 def on_recv_resp(channel, method, properties, body):
     global response
@@ -30,7 +56,15 @@ def read_channel(channel):
     resp = response
     response = None
     return json.loads(resp)
-    
+
+
+def write(data):
+    global count
+    #rdgA, rdgB, rdgC, rdgD, sensA, sensB, sensC, sensD = data
+    t_ds.resize(count+1, axis=0)
+    t_ds[count] = tuple(data)
+    count += 1
+
 if __name__ == '__main__':
     while True:
         t1 = time.time()
@@ -44,4 +78,4 @@ if __name__ == '__main__':
         print(d1, t1, t2)
         time.sleep(1)
         
-        #write({d1, abc, d2})
+        write(d1)
